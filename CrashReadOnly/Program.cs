26a15922bb95b07b61d97350b703e3e0489155a2
Leon <xiaoyankeji@hotmail.com>
2018-08-16 09:58:47 +0800

    添加项目文件。

---
+++ CrashReadOnly/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashReadOnly
+{
+    //旨在证明 C# 的只读只是对编译器的约束，可以IL反编译程序分析后通过System.Reflection修改自动属性的值；
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建演示对象
+            ReadOnlyDemo demo = new ReadOnlyDemo();
+
+            //获取私有字段实例并赋值
+            typeof(ReadOnlyDemo).GetField(
+                "Value_1",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance
+            )
+            .SetValue(demo, 111);
+
+            //获取私有自动属性的真实数据变量实例并赋值
+            typeof(ReadOnlyDemo).GetField(
+                "<Value_2>k__BackingField",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance
+            )
+            .SetValue(demo, 222);
+
+            //获取私有方法并调用
+            typeof(ReadOnlyDemo).GetMethod(
+                "Show",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance
+            )
+            .Invoke(demo, null);
+
+            Console.Read();
+        }
+    }
+}
---
+++ CrashReadOnly/ReadOnlyDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashReadOnly
+{
+    class ReadOnlyDemo
+    {
+        /// <summary>
+        /// 私有变量
+        /// </summary>
+        /// <remarks>IL: .field public initonly int32 Value_1</remarks>
+        private readonly int Value_1 = 1000;
+
+        /// <summary>
+        /// 只读自动属性
+        /// </summary>
+        /// <remarks>IL: 增加私有变量以记录自动属性的数据，.field private initonly int32 '<Value_2>k__BackingField'</remarks>
+        private int Value_2 { get; } = 2000;
+
+        /// <summary>
+        /// 私有输出方法
+        /// </summary>
+        private void Show() => Console.WriteLine($"Value_1 = {Value_1} \nValue_2 = {Value_2}");
+    }
+}
